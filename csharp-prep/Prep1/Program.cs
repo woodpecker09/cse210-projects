085286d15e2966a182e9d87fcbbb5e1b48a6bea4
woodpecker09 <ortegaharold99@outlook.es>
2024-09-19 23:18:33 -0600

    Finished prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,9 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("What is your first name?");
+        Console.Write("What is your first name? ");
         string firstName = Console.ReadLine();
-        Console.Write("What is your second name?");
+        Console.Write("What is your second name? ");
         string secondName = Console.ReadLine();
         Console.WriteLine($"Your name is {secondName}, {firstName} {secondName}.");
     }
