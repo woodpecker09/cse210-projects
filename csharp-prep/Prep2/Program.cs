d745c5fbafb6a9ca09f9b0815960089f73381de7
woodpecker09 <ortegaharold99@outlook.es>
2024-09-20 12:00:22 -0600

    Prep2 Finished

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,52 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What's your grade percentage? ");
+        int grade = int.Parse(Console.ReadLine());
+        string gradeLetter = "";
+        bool passGrade = true;
+        string sings = "";
+        if (grade >= 90)
+            {
+                gradeLetter = "A";
+            }
+        else if (grade >= 80)
+            {
+                gradeLetter = "B";
+            }
+        else if (grade >= 70)
+            {   
+                gradeLetter = "C";
+            }
+        else if (grade >= 60)
+            {
+                gradeLetter = "D";
+                passGrade = false;
+            }
+        else if (grade < 60)
+            {
+                gradeLetter = "F";
+                passGrade = false;
+            }
+        if (!(grade >= 94 || grade < 60))
+            {
+                if(grade % 10 >= 7)
+                    {
+                        sings = "+";
+                    }
+                else if (grade % 10 <= 3 )
+                    {
+                        sings = "-";
+                    }
+            }
+        if (passGrade)
+            {
+                Console.WriteLine($"Congratulations, you pass the class with a {gradeLetter}{sings}");
+            }
+        else
+            {
+                Console.WriteLine($"Sorry, you didn't pass the class, your grade is {gradeLetter}{sings}");
+            }
+        
     }
 }
