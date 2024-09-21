37be05f9ae89b451b09062a0b29aa08c056b362a
woodpecker09 <ortegaharold99@outlook.es>
2024-09-20 19:05:08 -0600

    Prep 3 Finished

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomNumber = new Random();
+
+        int magicNumber =  randomNumber.Next(1,101);
+        Console.WriteLine($"the magic number is{magicNumber}");
+        int guessNumber = 0;
+        bool continuePlaying = true;
+        int counting = 0;     
+
+        while(magicNumber != guessNumber || continuePlaying)
+            {
+                
+                counting += 1;
+                Console.Write("What is the magic number? ");
+                string guess = Console.ReadLine();
+                guessNumber = int.Parse(guess);
+                if (guessNumber > magicNumber)
+                    {
+                        Console.WriteLine("Lower");
+                    }
+                else if (guessNumber < magicNumber)
+                    {
+                        Console.WriteLine("Higher");
+                    }
+                else
+                    {
+                        Console.WriteLine($"You guessed it! it took {counting} times");
+                    }
+                if (magicNumber == guessNumber && continuePlaying)
+                    {
+                        Console.Write("Do you want to keep playing?");
+                        string keep = Console.ReadLine();
+                        if (keep == "no")
+                            {
+                                continuePlaying = false;
+                            }
+                    }
+            }
+        
     }
 }
