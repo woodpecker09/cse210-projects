ef2e9771c05743b57151340163ba7cf2a2a7485f
woodpecker09 <ortegaharold99@outlook.es>
2024-09-25 12:10:50 -0600

    Abstraction Learning Activity Finished

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Resume myResume = new Resume();
+        myResume._name = "Allison Rose";
+        
+        Job job1 = new Job();
+        myResume._jobs.Add(job1);
+        myResume._jobs[0]._company = "Microsoft";
+        myResume._jobs[0]._jobTitle = "Software Engineer";
+        myResume._jobs[0]._startYear = 2019;
+        myResume._jobs[0]._endYear = 2022;
+    
+        Job job2 = new Job();
+        myResume._jobs.Add(job2);
+        myResume._jobs[1]._company = "Apple";
+        myResume._jobs[1]._jobTitle = "Manager";
+        myResume._jobs[1]._startYear = 2022;
+        myResume._jobs[1]._endYear = 2023;
+
+        myResume.DisplayResume();
+
     }
 }
