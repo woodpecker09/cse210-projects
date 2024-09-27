14890757fb9142bb2c7a0071bb062d1adbfdf865
woodpecker09 <ortegaharold99@outlook.es>
2024-09-26 22:53:58 -0600

    Design Activity Done

---
+++ prove/Develop02/PromptGenerator.cs
@@ -0,0 +1,10 @@
+public class PromptGenerator
+    {
+        public List<string> _prompts = new List<string>() ;
+
+        public string GetRandomPrompt()
+            {
+                return "";
+            }
+
+    }
