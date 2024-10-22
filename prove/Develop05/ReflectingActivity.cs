4842dc4d6250f9f32b3d4f0d6502a2d0f8993d3d
woodpecker09 <ortegaharold99@outlook.es>
2024-10-21 21:36:48 -0600

    Develop 05 done

--- prove/Develop05/ReflectingActivity.cs
+++ prove/Develop05/ReflectingActivity.cs
@@ -24,7 +24,12 @@ public class ReflectingActivity : Activity
             "Why was this experience meaningful to you?",
             "Have you ever done anything like this before?",
             "How did you get started?",
-
+            "How did you feel when it was complete?",
+            "What made this time different than other times when you were not as successful?",
+            "What is your favorite thing about this experience?",
+            "What could you learn from this experience that applies to other situations?",
+            "What did you learn about yourself through this experience?",
+            "How can you keep this experience in mind in the future?",
         };
         _checkQuestionsList = new();
         _checkPromptsList = new();
